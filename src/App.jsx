5bd7339c8f4d1feb587cbebee2b5f0c48dd750e6
Stephen <stephenasupe@gmail.com>
2023-06-08 00:55:21 -0400

    small bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, 
+import { About, Contact, Feedbacks, Hero, 
   Navbar, Tech, Works, StarsCanvas }
  from "./components";
 const App = () => {
@@ -11,7 +11,6 @@ const App = () => {
         <Hero />
         </div>
         <About />
-        <Experience />
         <Tech />
         <Works />
         <Feedbacks />
